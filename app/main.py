1828675b0a09a4cb48177f6c1aaca32dabb6acc7
Ema Guillén <ema@guillen.com.ar>
2025-07-28 23:52:19 +0000

    cors config

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-from sqlmodel import SQLModel
+
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import auth, timer
 app = FastAPI(title="Taim Trackrr")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Include routers
