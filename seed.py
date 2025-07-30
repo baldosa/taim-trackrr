69b1e4d7e146685fe4e4c1a9fb871234a4622618
Ema Guillén <ema@guillen.com.ar>
2025-07-30 00:25:39 +0000

    try except seed

--- seed.py
+++ seed.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
+from sqlalchemy import exc
 from sqlmodel import Session
+from app import db
 from app.core.security import pwd_context
 from app.db.base import engine
 from app.db.models import User
@@ -8,16 +10,21 @@ from app.db.models import User
 load_dotenv()
 
 with Session(engine) as session:
-    user_data =User(
-        id=1,
-        username=os.getenv("DEFAULT_USERNAME", "admin"),
-        hashed_password=pwd_context.hash(os.getenv("DEFAULT_PASSWORD", "admin")),
-        email=os.getenv("DEFAULT_EMAIL", "user@user.com"),
-        full_name=os.getenv("DEFAULT_FULL_NAME", "user"),
-        disabled=False,
-    )
-    session.add(user_data)
-    session.commit()
-    session.refresh(user_data)
-    print(f"Superuser created: {user_data.username} with ID {user_data.id}")
-    session.close()
+    try:
+
+        user_data =User(
+            id=1,
+            username=os.getenv("DEFAULT_USERNAME", "admin"),
+            hashed_password=pwd_context.hash(os.getenv("DEFAULT_PASSWORD", "admin")),
+            email=os.getenv("DEFAULT_EMAIL", "user@user.com"),
+            full_name=os.getenv("DEFAULT_FULL_NAME", "user"),
+            disabled=False,
+        )
+        session.add(user_data)
+        session.commit()
+        session.refresh(user_data)
+        print(f"Superuser created: {user_data.username} with ID {user_data.id}")
+        session.close()
+    except exc.IntegrityError:
+        print("Superuser already exists, skipping creation.")
+        session.rollback()
